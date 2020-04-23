3ee49cf4a00047d848554aa7712bcdb9fc9bb468
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-23 12:27:05 +0500

    format index.js file

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 import ActionSheet from "./src";
-import {DeviceEventEmitter} from "react-native";
- /**
-   * Adds an event listener for when ActionSheet reach its maximum height.
-   * 
-   * @param callback Function to execute.
-   * 
-   */
+import { DeviceEventEmitter } from "react-native";
+/**
+ * Adds an event listener for when ActionSheet reach its maximum height.
+ *
+ * @param callback Function to execute.
+ *
+ */
 
-  export const addHasReachedTopListener = (callback) => {
+export const addHasReachedTopListener = (callback) => {
+  return DeviceEventEmitter.addListener("hasReachedTop", callback);
+};
 
-    return DeviceEventEmitter.addListener('hasReachedTop', callback)
- 
-   }
- 
- 
-   /**
-    * Remove addHasReachedTopListener.
-    * 
-    * @param callback Function to execute.
-    * 
-    */
- 
-  export const removeHasReachedTopListener = (callback) => {
- 
-    return DeviceEventEmitter.removeListener('hasReachedTop', callback)
- 
-   }
+/**
+ * Remove addHasReachedTopListener.
+ *
+ * @param callback Function to execute.
+ *
+ */
 
+export const removeHasReachedTopListener = (callback) => {
+  return DeviceEventEmitter.removeListener("hasReachedTop", callback);
+};
 
 export default ActionSheet;
