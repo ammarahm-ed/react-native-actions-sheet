ddc2cd3fafafd4fe92c9713073e591a9dbc6e92e
ammarahm-ed <ammarahmed6506@gmail.com>
2022-08-20 00:56:06 +0500

    add expo example

---
+++ expo-example/src/sheets.ts
@@ -0,0 +1,17 @@
+import ExampleSheet from "./examplesheet";
+import ConfirmSheet from "./confirm";
+import { registerSheet } from "react-native-actions-sheet";
+
+/**
+ * Registering the sheets here because otherwise sheet closes on
+ * hot reload during development.
+ */
+registerSheet("confirm-sheet", ConfirmSheet);
+registerSheet("example-sheet", ExampleSheet);
+
+export {};
+
+/**
+ * Since we are not importing our Sheets in any component or file, we want to make sure
+ * they are bundled by the JS bundler. Hence we will import this file in App.js.
+ */
