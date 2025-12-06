b714bda3fb99b04c125805513b2e5b59853b9def
Myroslav Hryschenko <miroslavgrischenko@gmail.com>
2025-12-06 23:01:40 +0100

    add favicon files and update theme.config.js for improved documentation visuals

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -31,6 +31,11 @@ export default {
         content="A Cross Platform(Android & iOS) ActionSheet with a robust and flexible api, native performance and zero dependency code for react native. Create anything you want inside ActionSheet."
       />
       <meta name="og:title" content="ActionSheet for React Native" />
+      <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
     </>
   ),
 };
